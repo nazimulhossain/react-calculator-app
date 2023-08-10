20e9877116484336f4ef4ca069ecbafaabfc66d7
nazimulhossain <nhossain10@gmail.com>
2023-08-10 20:31:10 +0530

    format floating point number to show 2 digit after decimal

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ export default function App() {
       case '/':
         result = op1 / op2;
 
-        setOperand(result);
+        setOperand(result.toFixed(2));
         break;
 
       default:
